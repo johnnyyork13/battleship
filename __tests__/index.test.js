2181477ab794b471618ec2c2f43b38cc3d4f0087
johnnyyork13 <johnnyyork13@hotmail.com>
2023-05-12 14:10:27 -0400

    refactoring code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,83 +1,15 @@
-const {Ship, Gameboard, Player} = require('../src/index');
+const {Ship} = require('../src/index');
+const myShip = new Ship(3);
 
-const shipFactory = () => Object.create(Ship);
-const gameboardFactory = () => Object.create(Gameboard);
-const myGameboard = gameboardFactory();
-const newPlayer = () => Object.create(Player);
-const playerOne = newPlayer();
-
-test('Ship Length Default', () => {
-    const myShip = shipFactory()
-    expect(myShip.length).toBe(null);
+test('Check Length', () => {
+    expect(myShip.length).toBe(3);
 })
-test('Ship Hit', () => {
-    const myShip = shipFactory();
+test('Hit Ship', () => {
     myShip.hit();
     expect(myShip.hitCount).toBe(1);
 })
-test('Sunk Ship', () => {
-    const myShip = shipFactory();
-    myShip.length = 3;
-    myShip.hit();
+test('Sink Ship', () => {
     myShip.hit();
     myShip.hit();
     expect(myShip.isSunk).toBe(true);
-}) 
-
-test('Placed Ship X', () => {
-    const myShip = shipFactory();
-    playerOne.symbol = 1;
-    myShip.length = 2;
-    myGameboard.addShipToGrid(playerOne, myShip, [0,0]);
-    const testGrid = [
-        [myShip,myShip,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]
-    ]
-    expect(myGameboard.grid).toStrictEqual(testGrid);
-})
-
-test('Placed Ship Y', () => {
-    const myShip = shipFactory();
-    playerOne.symbol = 1;
-    myShip.orientation = 'y';
-    myShip.length = 3;
-    myGameboard.addShipToGrid(playerOne, myShip, [3,2]);
-    const testGrid = [
-        [myShip,myShip,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,myShip,0,0,0,0,0,0],
-        [0,0,0,myShip,0,0,0,0,0,0],
-        [0,0,0,myShip,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]
-    ]
-    expect(myGameboard.grid).toStrictEqual(testGrid);
-})
-
-test('Receive Attack', () => {
-    const testGrid = [
-        [1,1,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]
-    ]
-    myGameboard.receiveAttack([0,0]);
-    expect(myGameboard.grid).toStrictEqual(testGridAttack);
 })
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const Ship = {
-    player: null,
-    length: null,
-    hitCount: 0,
-    isSunk: false,
-    orientation: 'x',
+class Ship {
+    constructor(length){
+        this.length = length;
+        this.hitCount = 0;
+        this.isSunk = false;
+        this.orientation = 'x';
+    }
+
     hit() {
         this.hitCount++;
         if (this.hitCount === this.length) {
@@ -12,59 +14,10 @@ const Ship = {
     }
 }
 
-const Gameboard = {
-    grid: [
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]
-    ],
-    addShipToGrid(player, ship, loc){
-        ship.player = player;
-        for (let y = 0; y < this.grid.length; y++) {
-            for (let x = 0; x < this.grid[y].length; x++) {
-                if (ship.orientation === 'x' &&
-                    y === loc[1] &&
-                    (x >= loc[0] &&
-                    x < loc[0] + ship.length)) {
-                        this.grid[y][x] = ship;
-                } else if (ship.orientation === 'y' && 
-                    x === loc[0] &&
-                    (y >= loc[1] && y < loc[1] + ship.length)) {
-                        this.grid[y][x] = ship;
-                    }
-            }
-        }
-    },
-    receiveAttack(loc){
-        const coord = this.grid[loc[1]][loc[0]];
-        if (coord === 0) {
-            this.grid[loc[1]][loc[0]] = 1;
-        } else if (coord.player !== null) {
-            coord.hitCount++;
-            this.grid[loc[1]][loc[0]] = 'X';
-        }
-    }
-}
-
-const Player = {
-    name: null,
-    symbol: null,
-    score: 0,
-}
-
 
 
 
 
 module.exports = {
-    Ship,
-    Gameboard,
-    Player
+    Ship
 }
