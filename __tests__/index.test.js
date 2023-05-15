20cbf7491d3299bce5469e8252d574b176e7570b
johnnyyork13 <johnnyyork13@hotmail.com>
2023-05-15 09:41:14 -0400

    add tests for gameboard

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,72 @@
-const {Ship} = require('../src/index');
-const myShip = new Ship(3);
+const {Ship, Gameboard} = require('../src/index');
+const myShipX = new Ship(3);
+const myShipY = new Ship(3);
+myShipY.orientation = 'y';
+const myGameboard = new Gameboard();
+
+const testBoard = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+]
 
 test('Check Length', () => {
-    expect(myShip.length).toBe(3);
+    expect(myShipX.length).toBe(3);
 })
+
+test('Check Gameboard', () => {
+    expect(myGameboard.board).toStrictEqual(testBoard);
+})
+
+test('Place Ship X', () => {
+    myGameboard.place(myShipX, [0,0]);
+    expect(myGameboard.board[0][0]).toBe(myShipX);
+})
+
+test('Whole Ship is Present X', () => {
+    expect(myGameboard.board[0][2]).toBe(myShipX);
+    expect(myGameboard.board[0][3]).toBe(0);
+})
+
+test('Place Ship Y', () => {
+    myGameboard.place(myShipY, [1,1]);
+    expect(myGameboard.board[1][1]).toBe(myShipY);
+})
+
+test('Whole Ship is Present Y', () => {
+    expect(myGameboard.board[3][1]).toBe(myShipY);
+    expect(myGameboard.board[4][1]).toBe(0);
+})
+
 test('Hit Ship', () => {
-    myShip.hit();
-    expect(myShip.hitCount).toBe(1);
+    myGameboard.receiveAttack([0, 0]);
+    expect(myShipX.hitCount).toBe(1);
+    expect(myGameboard.board[0][0]).toBe('hit');
 })
-test('Sink Ship', () => {
-    myShip.hit();
-    myShip.hit();
-    expect(myShip.isSunk).toBe(true);
+
+test('Miss Ship', () => {
+    myGameboard.receiveAttack([3, 5]);
+    expect(myGameboard.board[5][3]).toBe('miss');
 })
+
+test('All Ships Sunk', () => {
+    myGameboard.receiveAttack([1, 0]);
+    myGameboard.receiveAttack([2, 0]);
+    myGameboard.receiveAttack([1, 1]);
+    myGameboard.receiveAttack([1, 2]);
+    myGameboard.receiveAttack([1, 3]);
+    expect(myGameboard.checkShipsSunk()).toBe(true);
+})
+// test('Sink Ship', () => {
+//     myShipX.hit();
+//     myShipX.hit();
+//     expect(myShipX.isSunk).toBe(true);
+// })
+
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ class Ship {
         this.hitCount = 0;
         this.isSunk = false;
         this.orientation = 'x';
+        this.ship = this;
     }
 
     hit() {
@@ -14,10 +15,69 @@ class Ship {
     }
 }
 
+class Gameboard {
+    constructor() {
+        this.board = [
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0]
+        ]
+    }
 
+    place(ship, [coordX, coordY]) {
+        for (let y = 0; y < this.board.length; y++ ) {
+            for (let x = 0; x < this.board[y].length; x++) {
+                if (ship.orientation === 'x') {
+                    if (y === coordY && (
+                        x >= coordX && 
+                        x < coordX + ship.length
+                    )) {
+                        this.board[y][x] = ship.ship;
+                    }
+                } else {
+                    if (x === coordX && (
+                        y >= coordY &&
+                        y < coordY + ship.length
+                    )) {
+                        this.board[y][x] = ship.ship;
+                    }
+                }
+                
+            }
+        }
+    }
+
+    receiveAttack([coordX, coordY]) {
+        if (this.board[coordY][coordX] !== 0) {
+            this.board[coordY][coordX].hit();
+            this.board[coordY][coordX] = 'hit';
+        } else {
+            this.board[coordY][coordX] = 'miss';
+        }
+    }
+
+    checkShipsSunk() {
+        for (let y = 0; y < this.board.length; y++) {
+            for (let x = 0; x < this.board[y].length; x++) {
+                if (typeof this.board[y][x] === 'object') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
 
 
 
 module.exports = {
-    Ship
+    Ship,
+    Gameboard
 }
